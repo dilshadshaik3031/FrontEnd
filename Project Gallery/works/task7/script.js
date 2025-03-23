54c5559bb5057f64810c77c2a902c52a8bbd13cf
dilshad <dilshadshaik3031@gmail.com>
2025-03-22 21:57:51 -0500

    pushing dynamic project to git

---
+++ Project Gallery/works/task7/script.js
@@ -0,0 +1,17 @@
+let x = 10;
+console.log("X value: " + x);
+
+let y = x + 5;
+console.log("Addition of x and y: " + y);
+
+let z = y - x;
+console.log("Subtraction of y and x: " + z);
+
+let p = x * z;
+console.log("Multiplication of x and z: " + p);
+
+let q = p / x;
+console.log("Division of p and x: " + q);
+
+let r = x ** q;
+console.log("Modulo of x and q: " + r);
---
+++ Project Gallery/works/task8/string_script.js
@@ -0,0 +1,6 @@
+function update() {
+	const inputField = document.getElementById("input_field");
+	const outputField = document.getElementById("output_field");
+	outputField.value = inputField.value.toUpperCase();
+	}
+	
