19726a4c2ae9d1141c32211085a6544793c316f8
dilshad <dilshadshaik3031@gmail.com>
2025-03-22 21:59:15 -0500

    no message

--- Project Gallery/works/task8/string_script.js
+++
@@ -1,6 +0,0 @@
-function update() {
-	const inputField = document.getElementById("input_field");
-	const outputField = document.getElementById("output_field");
-	outputField.value = inputField.value.toUpperCase();
-	}
-	
